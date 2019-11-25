fa0591b8f01fe3aa7779427b60ab2e523f6fb9f0
Eji1700 <regulus87@yahoo.com>
2019-11-24 20:22:17 -0800

    Add project files.

---
+++ Cards/Types.fs
@@ -0,0 +1,18 @@
+﻿namespace Types
+
+module Cards =
+
+    type Suits =
+        | Spades
+        | Hearts
+        | Clubs
+        | Diamonds
+
+    type Faces =
+        | Two | Three | Four | Five | Six | Seven
+        | Eight | Nine | Ten | Jack | Queen | King | Ace
+
+    let (|Red|Black|) suit =
+        match suit with
+        | Diamonds | Hearts -> Red
+        | Clubs | Spades -> Black
